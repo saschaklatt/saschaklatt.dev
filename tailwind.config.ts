5ac86727ac2b2d8c213434d2ba1ba3fe646d98b8
Sascha Klatt <sascha@appocados.com>
2023-09-04 16:08:17 +0700

    [SK-78] improve animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -64,6 +64,10 @@ export default {
                 "xl": `0 20px 13px rgb(0 0 0 / 0.03)) drop-shadow(0 8px 5px rgb(0 0 0 / 0.08)`,
                 "2xl": `0 25px 25px rgb(0 0 0 / 0.3)`,
             },
+            transitionProperty: {
+                spacing: "margin, padding",
+                fonts: "font-size, font-weight",
+            },
         },
     },
     plugins: [],
