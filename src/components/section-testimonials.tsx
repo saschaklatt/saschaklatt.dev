10d69b23cef41bc311c0b026bc25c8be74b38863
Sascha Klatt <sascha@appocados.com>
2023-09-06 11:18:30 +0700

    [SK-80] style testimonial section for mobile and desktop

--- src/components/section-testimonials.tsx
+++ src/components/section-testimonials.tsx
@@ -1,5 +1,5 @@
 import type {Testimonial} from "../data/typedefs";
-import {useState} from "react";
+import {useMemo, useState} from "react";
 import {AnimatePresence, motion} from "framer-motion";
 import {AnimatedHeight} from "./animated-height";
 
@@ -10,72 +10,73 @@ interface SectionTestimonialProps {
 }
 
 const SectionTestimonial = ({headline, testimonials, name}: SectionTestimonialProps) => {
-    const initialSlideId = `slide-0`;
-    const [selectedSlideId, setSelectedSlideId] = useState(initialSlideId);
-
-    const slides = testimonials.map((testimonial, idx) => ({
-        ...testimonial,
-        active: `slide-${idx}` === selectedSlideId,
-        id: `slide-${idx}`,
-    }));
-
-    const handleRadioChange: React.ChangeEventHandler<HTMLInputElement> = (evt) => {
-        console.log(evt.target.id);
-        setSelectedSlideId(evt.target.id);
-    };
+    const slides = useMemo(() => testimonials.map((testimonial, idx) => ({...testimonial, id: `slide-${idx}`})), []);
+    const [selectedIdx, setSelectedIdx] = useState(0);
+    const selectedSlide = slides[selectedIdx];
 
     return (
         <section className="my-32 md:my-80">
             <div className="content flex flex-col items-center">
-                <h2 className="font-serif text-4xl md:text-6xl flex flex-col items-center">{headline}</h2>
+                <h2 className="font-serif text-4xl md:text-5xl flex flex-col items-center md:mb-20">{headline}</h2>
 
-                {/* Slides --> */}
-                <AnimatedHeight>
-                    <ul className="py-20 grid-cols-1 grid-rows-1 grid">
-                        {slides.map(({id, text, active}) => (
-                            <AnimatePresence key={id} initial={false} mode="popLayout">
-                                {active && (
-                                    <motion.li
-                                        key={id}
-                                        initial={{opacity: 0, transform: "translateY(-20%)"}}
-                                        animate={{opacity: 1, transform: "translateY(0)"}}
-                                        exit={{opacity: 0, transform: "translateY(20%)"}}
-                                        className={`w-full flex justify-center items-center col-start-1 col-span-1 row-start-1 row-span-1`}
-                                    >
-                                        <p className="font-thin max-w-prose text-2xl opacity-75 text-center italic">{text}</p>
-                                    </motion.li>
-                                )}
-                            </AnimatePresence>
-                        ))}
-                    </ul>
-                </AnimatedHeight>
+                <div className="max-w-prose text-base md:text-2xl bg-[url('/img/bg-heart.svg')] bg-no-repeat bg-center">
+                    {/* Slides --> */}
+                    <AnimatedHeight>
+                        <ul className="py-10 md:py-20 grid-cols-1 grid-rows-1 grid">
+                            {slides.map((slide) => (
+                                <AnimatePresence key={slide.id} initial={false} mode="popLayout">
+                                    {slide === selectedSlide && (
+                                        <motion.li
+                                            key={slide.id}
+                                            initial={{opacity: 0, transform: "translateY(-20%)"}}
+                                            animate={{opacity: 1, transform: "translateY(0)"}}
+                                            exit={{opacity: 0, transform: "translateY(20%)"}}
+                                            className={`w-full flex justify-center items-center col-start-1 col-span-1 row-start-1 row-span-1`}
+                                        >
+                                            <p className="font-thin opacity-90 text-center">{slide.text}</p>
+                                        </motion.li>
+                                    )}
+                                </AnimatePresence>
+                            ))}
+                        </ul>
+                    </AnimatedHeight>
+
+                    {/* Author */}
+                    <section className="text-right self-end mb-14">
+                        <h3 className="text-sm md:text-xl font-semibold">{`${selectedSlide.referrer.firstName} ${selectedSlide.referrer.lastName}`}</h3>
+                        <p className="text-xs md:text-base font-thin uppercase opacity-75">{`${selectedSlide.referrerRole}`}</p>
+                        {selectedSlide.referrerCompany && (
+                            <p className="text:xs md:text-base font-thin opacity-75">{selectedSlide.referrerCompany.name}</p>
+                        )}
+                    </section>
+                </div>
 
                 {/*  Avatars */}
-                <ul className="inline-flex flex-wrap gap-4 items-center h-28">
-                    {slides.map(({referrer, id, active}) => (
-                        <li key={id} className={`text-center h-28 flex items-center`}>
+                <ul className="inline-flex flex-wrap gap-4 items-center justify-center">
+                    {slides.map((slide, idx) => (
+                        <li key={slide.id} className={`text-center flex items-center`}>
                             <input
                                 type="radio"
                                 name={name}
-                                id={id}
-                                checked={active}
-                                onChange={handleRadioChange}
+                                id={slide.id}
+                                checked={slide === selectedSlide}
+                                onChange={() => setSelectedIdx(idx)}
                                 className="sr-only common-focus-label-after"
                             />
                             <label
-                                htmlFor={id}
-                                className={`cursor-pointer inline-block rounded-full overflow-hidden transition-all 
+                                htmlFor={slide.id}
+                                className={`cursor-pointer inline-block rounded-full overflow-hidden transition-all h-10 w-10 md:w-16 md:h-16
                                 ${
-                                    active
-                                        ? "w-28 h-28 opacity-100 shadow-2xl"
-                                        : "w-20 h-20 opacity-50 md:hover:scale-110 shadow-md"
+                                    slide === selectedSlide
+                                        ? "scale-150 opacity-100 shadow-2xl"
+                                        : "opacity-40 md:hover:scale-110 shadow-md"
                                 }`}
                             >
                                 <img
-                                    src={referrer.image.src}
-                                    width={referrer.image.width}
-                                    height={referrer.image.height}
-                                    alt={`Profile picture of ${referrer.firstName} ${referrer.lastName}`}
+                                    src={slide.referrer.image.src}
+                                    width={slide.referrer.image.width}
+                                    height={slide.referrer.image.height}
+                                    alt={`Profile picture of ${slide.referrer.firstName} ${slide.referrer.lastName}`}
                                 />
                             </label>
                         </li>
