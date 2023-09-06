c44c1e418be5a7261c6122a8d2d57782e9af4710
Sascha Klatt <sascha@appocados.com>
2023-09-06 15:03:53 +0700

    [SK-80] add indicator element to avatars

--- src/components/section-testimonials.tsx
+++ src/components/section-testimonials.tsx
@@ -22,7 +22,7 @@ const SectionTestimonial = ({headline, testimonials, name}: SectionTestimonialPr
                         src="/img/quote.svg"
                         width="60"
                         height="60"
-                        className="absolute top-2 md:top-0 left-0 w-10 h-10 md:w-20 md:h-20"
+                        className="absolute top-2 hidden md:block md:top-0 left-0 w-10 h-10 md:w-20 md:h-20"
                     />
 
                     {/* Slides --> */}
@@ -34,8 +34,8 @@ const SectionTestimonial = ({headline, testimonials, name}: SectionTestimonialPr
                                 className={[
                                     `w-full flex justify-center items-center col-start-1 col-span-1 row-start-1 row-span-1 transition-all duration-300`,
                                     slide === selectedSlide
-                                        ? "opacity-100 translate-y-0 visible delay-200"
-                                        : "opacity-0 -translate-y-8 invisible",
+                                        ? "opacity-100 scale-100 visible delay-200"
+                                        : "opacity-0 scale-75 invisible",
                                 ].join(" ")}
                             >
                                 <p className="font-thin opacity-90 text-center">{slide.text}</p>
@@ -54,7 +54,16 @@ const SectionTestimonial = ({headline, testimonials, name}: SectionTestimonialPr
                 {/*  Avatars */}
                 <ul className="inline-flex flex-wrap gap-4 md:gap-6 items-center justify-center">
                     {slides.map((slide, idx) => (
-                        <li key={slide.id} className={`text-center flex items-center`}>
+                        <li
+                            key={slide.id}
+                            className={[
+                                "relative text-center flex items-center",
+                                "after:transition-all after:duration-300",
+                                "after:content-[' '] after:absolute after:w-1/2 after:h-1 after:bg-accent after:-bottom-5 after:left-1/2 after:block after:rounded-full after:-translate-x-1/2",
+                                "after:md:h-1.5 after:md:-bottom-8",
+                                slide === selectedSlide ? "after:opacity-100 after:scale-100" : "after:opacity-0 after:scale-75",
+                            ].join(" ")}
+                        >
                             <input
                                 type="radio"
                                 name={name}
