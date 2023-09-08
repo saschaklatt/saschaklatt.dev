ed513bcea9e480ace99304fdb4787f2ff5099d79
Sascha Klatt <sascha@appocados.com>
2023-09-08 18:31:16 +0700

    [SK-94] move testimonial authors above the text

--- src/components/section-testimonials.tsx
+++ src/components/section-testimonials.tsx
@@ -27,7 +27,62 @@ const SectionTestimonial = ({headline, testimonials, name}: SectionTestimonialPr
     return (
         <section className="my-32 md:my-80">
             <div className="content flex flex-col items-center">
-                <h2 className="font-serif text-center text-4xl md:text-5xl flex flex-col items-center md:mb-20">{headline}</h2>
+                <h2 className="font-serif text-center text-4xl md:text-5xl flex flex-col items-center mb-20 md:mb-32">
+                    {headline}
+                </h2>
+
+                {/*  Avatars */}
+                <ul className="inline-flex flex-wrap gap-4 md:gap-6 items-center justify-center md:mb-10">
+                    {slides.map((slide, idx) => (
+                        <li
+                            key={slide.id}
+                            className={[
+                                "relative text-center flex items-center",
+                                slide === selectedSlide
+                                    ? "after:opacity-100 after:scale-100 after:animate-pulse"
+                                    : "after:opacity-0 after:scale-75",
+                            ].join(" ")}
+                        >
+                            <input
+                                type="radio"
+                                name={name}
+                                id={slide.id}
+                                checked={slide === selectedSlide}
+                                onChange={() => handleChange(idx)}
+                                className="sr-only common-focus-label-after"
+                            />
+                            <label
+                                htmlFor={slide.id}
+                                className={[
+                                    "cursor-pointer no-tap-highlight inline-block rounded-full overflow-hidden transition-all",
+                                    slide === selectedSlide
+                                        ? "scale-150 opacity-100 shadow-2xl"
+                                        : "opacity-40 md:hover:scale-110 shadow-md",
+                                ].join(" ")}
+                            >
+                                <picture>
+                                    <source
+                                        srcSet={`${slide.referrer.image60.src}, ${slide.referrer.image60_2x.src} 2x`}
+                                        media={`(max-width: ${numToPx(breakpoints.md)})`}
+                                    />
+                                    <source
+                                        srcSet={`${slide.referrer.image96.src}, ${slide.referrer.image96_2x.src} 2x`}
+                                        media={`(min-width: ${numToPx(breakpoints.md + 1)})`}
+                                    />
+                                    <img
+                                        src={slide.referrer.image96.src}
+                                        alt={`${slide.referrer.firstName} ${slide.referrer.lastName}`}
+                                        className="w-10 h-10 md:w-16 md:h-16"
+                                        width="96"
+                                        height="96"
+                                        loading="lazy"
+                                        draggable="false"
+                                    />
+                                </picture>
+                            </label>
+                        </li>
+                    ))}
+                </ul>
 
                 <div className="relative max-w-prose text-base md:text-2xl bg-[url('/img/bg-heart.svg')] bg-no-repeat bg-center">
                     <img
@@ -35,17 +90,17 @@ const SectionTestimonial = ({headline, testimonials, name}: SectionTestimonialPr
                         alt=""
                         width="60"
                         height="60"
-                        className="absolute top-2 hidden md:block md:top-0 left-0 w-10 h-10 md:w-20 md:h-20"
+                        className="absolute hidden md:block md:-top-8 left-0 w-10 h-10 md:w-20 md:h-20"
                     />
 
                     {/* Slides --> */}
-                    <ul className="pt-10 pb-4 md:py-20 grid-cols-1 grid-rows-1 grid">
+                    <ul className="pt-4 md:pt-10 pb-4 md:py-20 grid-cols-1 grid-rows-1 grid">
                         {slides.map((slide) => (
                             <li
                                 key={slide.id}
                                 aria-hidden={slide !== selectedSlide}
                                 className={[
-                                    `w-full flex justify-center items-center col-start-1 col-span-1 row-start-1 row-span-1 transition-all duration-300`,
+                                    `relative w-full flex justify-center items-center col-start-1 col-span-1 row-start-1 row-span-1 transition-all duration-300`,
                                     slide === selectedSlide
                                         ? "opacity-100 scale-100 visible delay-200"
                                         : "opacity-0 scale-75 invisible",
@@ -63,60 +118,6 @@ const SectionTestimonial = ({headline, testimonials, name}: SectionTestimonialPr
                         <p className="text-xs md:text-base font-light opacity-75">{selectedSlide.referrerCompany}</p>
                     </section>
                 </div>
-
-                {/*  Avatars */}
-                <ul className="inline-flex flex-wrap gap-4 md:gap-6 items-center justify-center">
-                    {slides.map((slide, idx) => (
-                        <li
-                            key={slide.id}
-                            className={[
-                                "relative text-center flex items-center",
-                                "after:transition-all after:duration-300",
-                                "after:content-[' '] after:absolute after:w-1/2 after:h-1 after:bg-accent after:-bottom-5 after:left-1/2 after:block after:rounded-full after:-translate-x-1/2",
-                                "after:md:h-1.5 after:md:-bottom-8",
-                                slide === selectedSlide
-                                    ? "after:opacity-100 after:scale-100 after:animate-pulse"
-                                    : "after:opacity-0 after:scale-75",
-                            ].join(" ")}
-                        >
-                            <input
-                                type="radio"
-                                name={name}
-                                id={slide.id}
-                                checked={slide === selectedSlide}
-                                onChange={() => handleChange(idx)}
-                                className="sr-only common-focus-label-after"
-                            />
-                            <label
-                                htmlFor={slide.id}
-                                className={`cursor-pointer no-tap-highlight inline-block rounded-full overflow-hidden transition-all h-10 w-10 md:w-16 md:h-16
-                                ${
-                                    slide === selectedSlide
-                                        ? "scale-150 opacity-100 shadow-2xl"
-                                        : "opacity-40 md:hover:scale-110 shadow-md"
-                                }`}
-                            >
-                                <picture>
-                                    <source
-                                        srcSet={`${slide.referrer.image60.src}, ${slide.referrer.image60_2x.src} 2x`}
-                                        media={`(max-width: ${numToPx(breakpoints.md)})`}
-                                    />
-                                    <source
-                                        srcSet={`${slide.referrer.image96.src}, ${slide.referrer.image96_2x.src} 2x`}
-                                        media={`(min-width: ${numToPx(breakpoints.md + 1)})`}
-                                    />
-                                    <img
-                                        src={slide.referrer.image96.src}
-                                        alt={`${slide.referrer.firstName} ${slide.referrer.lastName}`}
-                                        width="96"
-                                        height="96"
-                                        loading="lazy"
-                                    />
-                                </picture>
-                            </label>
-                        </li>
-                    ))}
-                </ul>
             </div>
         </section>
     );
