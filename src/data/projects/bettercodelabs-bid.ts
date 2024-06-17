21c8146fd5fdceaebd6085bec2f5041560d72872
Sascha Klatt <hello@saschaklatt.dev>
2024-06-17 12:48:56 +0700

    [SK-114] fix typos

--- src/data/projects/bettercodelabs-bid.ts
+++ src/data/projects/bettercodelabs-bid.ts
@@ -10,7 +10,7 @@ import image4x from "../../assets/images/project-bettercodelabs-bid@4x.webp";
 export const BettercodeBID: Project = {
     id: "bettercodelabs-bid",
     title: "bettercode BID",
-    subline: "UX/UI design and frontend development for a bidding platform.",
+    subline: "UX/UI design and front-end development for a bidding platform.",
     description: `Commissioned by bettercodeLABS to enhance the UI of their existing prototype into a production-ready design, I utilized Figma to create a user-centric design prioritizing simplicity and functionality. Through wireframing and prototyping, I developed a clickable prototype in Figma for desktop and mobile devices, ensuring an intuitive interface. The implementation of the design into the client application involved React.js and Material UI for a polished and consistent look. Real-time updates were seamlessly integrated via a REST API, elevating user engagement and responsiveness. The result is an optimized UI that enhances the overall bidding experience.`,
     client: BettercodeLABS,
     contractType: "freelance",
--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -13,7 +13,7 @@ export const allTestimonials: Testimonial[] = [
         referrer: FelixWolf,
         referrerRole: "Online Marketing Manager",
         referrerCompany: Studitemps.name,
-        text: "Sascha supported us with the relaunch of our corporate website jobvalley.com. We benefited greatly from his broad and deep expertise in frontend and backend development, as well as in UX/UI. Thanks to his wealth of experience, he was always able to find a suitable solution even for complex problems. With his friendly and professional manner, he quickly integrated himself into the project team. We could rely on him 100% and I would be very happy to work with him again.",
+        text: "Sascha supported us with the relaunch of our corporate website jobvalley.com. We benefited greatly from his broad and deep expertise in front-end and back-end development, as well as in UX/UI. Thanks to his wealth of experience, he was always able to find a suitable solution even for complex problems. With his friendly and professional manner, he quickly integrated himself into the project team. We could rely on him 100% and I would be very happy to work with him again.",
     },
     {
         referrer: MartinWilmer,
